bee094e587959901363cb10184154285b0d2c5ac
jhon <hankgit-h@hotmail.com>
2022-09-03 21:04:36 -0500

    choosing the winner

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // The Odin Project
 
 function game() {
+
+	const resultPlayer = [];
+	const resultComputer = [];
+
 	for (let i = 0; i < 5; i++) {
 		const playerSelection = prompt("Choice", "Rock, Papper, Scissors");
 
@@ -12,6 +16,9 @@ function game() {
 		}
 
 		const computerSelection = getComputer();
+		
+		//const resultPlayer = [];
+		//const resultComputer = [];
 
 		function playRound(playerSelection, computerSelection) {
 	
@@ -19,12 +26,14 @@ function game() {
 		
 				if(computerSelection == "Papper") {
 					console.log("You Lose! Papper beats Rock");
+					resultComputer.push("w");
 				}
 				else if(computerSelection == "Rock") {
 					console.log("There are no winners");
 				}
 				else if(computerSelection == "Scissors") {
 					console.log("You Win! Rock beats Scissors");
+					resultPlayer.push("w");
 				}
 			}
 
@@ -35,18 +44,22 @@ function game() {
 				}
 				else if(computerSelection == "Rock") {
 					console.log("You Win! Papper beats Rock");
+					resultPlayer.push("w");
 				}
 				else if(computerSelection == "Scissors") {
 					console.log("You Lose! Scissors beats Papper");
+					resultComputer.push("w");
 				}
 			}
 
 			else if(playerSelection == "Scissors") {
 				if(computerSelection == "Papper") {
 					console.log("You Win! Scissors beats Papper");
+					resultPlayer.push("w");
 				}
 				else if(computerSelection == "Rock") {
 					console.log("You Lose! Rock beats Scissors");
+					resultComputer.push("w");
 				}
 				else if(computerSelection == "Scissors") {
 					console.log("There are no winers");
@@ -54,7 +67,26 @@ function game() {
 			}
 		}
 		playRound(playerSelection, computerSelection);
+
+		//if (resultPlayer.length > resultComputer.length) {
+		//	console.log("Has ganado a la maquina");
+		//}
+		//else {
+		//	console.log("La maquina ha ganado");
+		//}
+	}
+
+	if (resultPlayer.length > resultComputer.length) {
+		console.log("Has ganado a la maquina");
+	}
+	else if (resultPlayer.length == resultComputer.length) {
+		console.log("Nadie ha ganado");
+	}
+	else {
+		console.log("La maquina ha ganado");
 	}
 }
 
 game();
+
+//The Odin Project
