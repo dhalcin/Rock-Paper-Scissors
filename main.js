3a42b8e20d69de5c7c527e26de03bb313e47e16a
jhon <hankgit-h@hotmail.com>
2022-09-14 00:05:30 -0500

    Edit main.js

--- main.js
+++ main.js
@@ -1,92 +1,47 @@
 // The Odin Project
-
 function game() {
-
-	const resultPlayer = [];
-	const resultComputer = [];
+	let resultPlayer = 0;
+	let resultComputer = 0;
+	let notWiner = 0;
 
 	for (let i = 0; i < 5; i++) {
-		const playerSelection = prompt("Choice", "Rock, Papper, Scissors");
-
 		function getComputer() {
 			const myArray = ["Rock", "Papper", "Scissors"];
-			const luck = Math.floor(Math.random() * myArray.length);
-
+			let luck = Math.floor(Math.random() * myArray.length);
 			return myArray[luck];
 		}
-
 		const computerSelection = getComputer();
+		const playerSelection = prompt("Enter");
 		
-		//const resultPlayer = [];
-		//const resultComputer = [];
-
 		function playRound(playerSelection, computerSelection) {
-	
-			if(playerSelection == "Rock") {
-		
-				if(computerSelection == "Papper") {
-					console.log("You Lose! Papper beats Rock");
-					resultComputer.push("w");
-				}
-				else if(computerSelection == "Rock") {
-					console.log("There are no winners");
-				}
-				else if(computerSelection == "Scissors") {
-					console.log("You Win! Rock beats Scissors");
-					resultPlayer.push("w");
-				}
+			if (playerSelection == computerSelection) {
+				console.log("Empate")
+				notWiner++
 			}
-
-			else if(playerSelection == "Papper") {
-				
-				if(computerSelection == "Papper") {
-					console.log("There are no winers");
-				}
-				else if(computerSelection == "Rock") {
-					console.log("You Win! Papper beats Rock");
-					resultPlayer.push("w");
-				}
-				else if(computerSelection == "Scissors") {
-					console.log("You Lose! Scissors beats Papper");
-					resultComputer.push("w");
-				}
+			// player
+			if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+			(playerSelection == "Papper" && computerSelection == "Rock") ||
+			(playerSelection == "Scissors" && computerSelection == "Papper")) {
+				console.log("Gana el jugador");
+				resultPlayer++;
 			}
-
-			else if(playerSelection == "Scissors") {
-				if(computerSelection == "Papper") {
-					console.log("You Win! Scissors beats Papper");
-					resultPlayer.push("w");
-				}
-				else if(computerSelection == "Rock") {
-					console.log("You Lose! Rock beats Scissors");
-					resultComputer.push("w");
-				}
-				else if(computerSelection == "Scissors") {
-					console.log("There are no winers");
-				}
+			// computer
+			if ((computerSelection == "Rock" && playerSelection == "Scissors") ||
+			(computerSelection == "Papper" && playerSelection == "Rock") ||
+			(computerSelection == "Scissors" && playerSelection == "Papper")) {
+				console.log("Gana la maquina");
+				resultComputer++;
 			}
 		}
 		playRound(playerSelection, computerSelection);
-
-		//if (resultPlayer.length > resultComputer.length) {
-		//	console.log("Has ganado a la maquina");
-		//}
-		//else {
-		//	console.log("La maquina ha ganado");
-		//}
 	}
-
-	if (resultPlayer.length > resultComputer.length) {
-		console.log("Has ganado a la maquina");
-	}
-	else if (resultPlayer.length == resultComputer.length) {
-		console.log("Nadie ha ganado");
-	}
-	else {
-		console.log("La maquina ha ganado");
+	if (resultPlayer > resultComputer) {
+		document.write("Gana el jugador");
+	}else if (resultPlayer < resultComputer) {
+		document.write("Gana la maquina");
+	}else {
+		document.write("no hay ganadores");
 	}
 }
-
 game();
-
-//The Odin Project
+// The Odin Project
