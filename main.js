87dbf1077fe8318a376c98e42d3659f4642171a3
jhon <hankgit-h@hotmail.com>
2022-09-27 22:40:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,65 @@
 // The Odin Project
-function game() {
-	let resultPlayer = 0;
-	let resultComputer = 0;
-	let notWiner = 0;
+let resultPlayer = 0;
+let resultComputer = 0;
+let notWiners = 0;
 
-	for (let i = 0; i < 5; i++) {
-		function getComputer() {
-			const myArray = ["Rock", "Papper", "Scissors"];
-			let luck = Math.floor(Math.random() * myArray.length);
-			return myArray[luck];
-		}
-		const computerSelection = getComputer();
-		const playerSelection = prompt("Enter");
+function getComputer() {
+	const computerOption = ["Rock", "Papper", "Scissors"];
+	let choice = Math.floor(Math.random() * computerOption.length);
+	return computerOption[choice];
+}
 		
-		function playRound(playerSelection, computerSelection) {
-			if (playerSelection == computerSelection) {
-				console.log("Empate")
-				notWiner++
-			}
-			// player
-			if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
-			(playerSelection == "Papper" && computerSelection == "Rock") ||
-			(playerSelection == "Scissors" && computerSelection == "Papper")) {
-				console.log("Gana el jugador");
-				resultPlayer++;
-			}
-			// computer
-			if ((computerSelection == "Rock" && playerSelection == "Scissors") ||
-			(computerSelection == "Papper" && playerSelection == "Rock") ||
-			(computerSelection == "Scissors" && playerSelection == "Papper")) {
-				console.log("Gana la maquina");
-				resultComputer++;
-			}
-		}
-		playRound(playerSelection, computerSelection);
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection == computerSelection) {
+		notWiners++;
+		resultText.innerHTML = 'Tie';
 	}
-	if (resultPlayer > resultComputer) {
-		document.write("Gana el jugador");
-	}else if (resultPlayer < resultComputer) {
-		document.write("Gana la maquina");
-	}else {
-		document.write("no hay ganadores");
+	
+	if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+		(playerSelection == "Papper" && computerSelection == "Rock") ||
+		(playerSelection == "Scissors" && computerSelection == "Papper")) {
+			resultPlayer++;
+			resultText.innerHTML = 'Player : ' + resultPlayer + ' - ' + 'Computer : ' + resultComputer;
+		}
+	
+	if ((computerSelection == "Rock" && playerSelection == "Scissors") ||
+		(computerSelection == "Papper" && playerSelection == "Rock") ||
+		(computerSelection == "Scissors" && playerSelection == "Papper")) {
+			resultComputer++;
+			resultText.innerHTML = 'Computer : ' + resultComputer + ' - ' + 'Player : ' + resultPlayer;
 	}
 }
-game();
+
+function winner() {
+	if (resultPlayer == 5 || resultComputer == 5) {
+		if (resultPlayer == 5) {
+			printWinner.innerHTML = "Gana el jugador";
+		}else if(resultComputer == 5) {
+			printWinner.innerHTML = "Gana la maquina";
+		}
+	}
+}
+
+const rockBtn = document.querySelector('#rock');
+const papperBtn = document.querySelector('#papper');
+const scissorsBtn = document.querySelector('#scissors');
+const resultText = document.querySelector('#start-game');
+const printWinner = document.querySelector('#result');
+
+rockBtn.addEventListener('click', ()=> {
+	clickEvent('Rock');
+});
+papperBtn.addEventListener('click', ()=>{
+	clickEvent('Papper');
+});
+scissorsBtn.addEventListener('click', ()=> {
+	clickEvent('Scissors');
+});
+
+function clickEvent(playerSelection) {
+	const computerSelection = getComputer();
+	//console.log("selection computer is : " + computerSelection);
+	playRound(playerSelection, computerSelection);
+	winner();
+}
 // The Odin Project
